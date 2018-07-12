1870252fab53d0316191c409111627abc0a79865
dpozinen <dpozinen@gmail.com>
2018-07-12 23:54:29 +0300

    print fully wokring

--- src/lemin/Farm.java
+++ src/lemin/Farm.java
@@ -18,27 +18,38 @@ class Farm
     void print(int what)
     {
         if (ants != 0)
-            System.out.println("Ants count: "+ ants);        
-        System.out.println("Start: "+ start);        
-        System.out.println("End: "+ end);         
+            System.out.println("Ants count: "+ ants);
+        System.out.println("Start: "+ start);
+        System.out.println("End: "+ end);
         // print room list
-        if (what <= 1)
+        if (what >= 1)
             for (Room r : roomList)
                 System.out.println("Room name: "+r.name);
         // print link list
-        if (what <= 2)
+        if (what >= 2)
             for (ArrayList <Room> rList : linkList)
             {
                 String roomOne = rList.get(0).name;
-                System.out.println("Links for room: " + roomOne);
-                for (int i = 1; i < rList.size(); i++)
-                    System.out.println(roomOne + "-" + rList.get(i));
+                if (rList.size() > 1)
+                {
+                    System.out.println("Links for room: " + roomOne);
+                        for (int i = 1; i < rList.size(); i++)
+                            System.out.println(roomOne + "-" + rList.get(i).name);
+                    System.out.println();
+                }
             }
-        if (what <= 3)
+        // print path list 
+        if (what >= 3)
             for (Path p : pathList)
-                p.print();    
-
-
+                p.print();
     }
-    
+    static void fillLinkList()
+    {
+        for (Room r : roomList)
+        {
+            ArrayList <Room> newRoom = new ArrayList <Room>();
+            newRoom.add(r);
+            Farm.linkList.add(newRoom);
+        }
+    }
 }
